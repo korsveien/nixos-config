78888f378eeb6d2a2eb37f135f2f5383b919af50
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-03 22:28:48 +0200

    Add termite config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/env sh
 
+mkdir -p $HOME/.config
+
 ln -s /home/pederpus/nixos-config/configuration.nix /etc/nixos/configuration.nix
 ln -s /etc/nixos/hardware-configuration.nix /home/pederpus/nixos-config/hardware-configuration.nix
 
 ln -s /home/pederpus/nixos-config/dotfiles/profile /home/pederpus/.profile
 ln -s /home/pederpus/nixos-config/dotfiles/gitconfig /home/pederpus/.gitconfig
-ln -s /home/pederpus/nixos-config/dotfiles/xinitrc /home/pederpus/.xinitrc
+ln -s /home/pederpus/nixos-config/dotfiles/xsessionrc /home/pederpus/.xsessionrc
 ln -s /home/pederpus/nixos-config/dotfiles/Xmodmap /home/pederpus/.Xmodmap
+ln -s /home/pederpus/nixos-config/dotfiles/Xresources /home/pederpus/.Xresources
 
 git clone git@github.com:pederpus/neovim-config.git $XDG_CONFIG_HOME/nvim
 
