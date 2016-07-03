684f8ac11ab98db5cf9756acab4628dbed9fc430
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-03 23:56:53 +0200

    Allow unfree packages

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,8 @@
       ./hardware-configuration.nix
     ];
 
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
    environment.systemPackages = with pkgs; [
