ee68b6f4c56606f4b2bf216069b85244818804e4
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-03 22:02:52 +0200

    Add x-files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/env sh
 
-ln -s $HOME/nixos-config/configuration.nix /etc/nixos/configuration.nix
-ln -s /etc/nixos/hardware-configuration.nix $HOME/nixos-config/hardware-configuration.nix
+ln -s /home/pederpus/nixos-config/configuration.nix /etc/nixos/configuration.nix
+ln -s /etc/nixos/hardware-configuration.nix /home/pederpus/nixos-config/hardware-configuration.nix
 
-ln -s $HOME/nixos-config/dotfiles/gitconfig $HOME/.gitconfig
-ln -s $HOME/nixos-config/dotfiles/profile $HOME/.profile
+ln -s /home/pederpus/nixos-config/dotfiles/profile /home/pederpus/.profile
+ln -s /home/pederpus/nixos-config/dotfiles/gitconfig /home/pederpus/.gitconfig
+ln -s /home/pederpus/nixos-config/dotfiles/xinitrc /home/pederpus/.xinitrc
+ln -s /home/pederpus/nixos-config/dotfiles/Xmodmap /home/pederpus/.Xmodmap
+
+git clone git@github.com:pederpus/neovim-config.git $XDG_CONFIG_HOME/nvim
+
