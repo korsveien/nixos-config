e5b874ed67517193a0ba6eb3d3b0f7bae8d1c743
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-04 00:06:52 +0200

    Add xmobarrc

--- setup.sh
+++ setup.sh
@@ -8,13 +8,13 @@ rm $HOME/.gitconfig
 rm $HOME/.bashrc
 rm $HOME/.xsession
 rm $HOME/.Xmodmap
-rm $HOME/.Xresources
+rm $HOME/.Xdefaults
 
 ln -s $HOME/nixos-config/dotfiles/bashrc $HOME/.bashrc
 ln -s $HOME/nixos-config/dotfiles/gitconfig $HOME/.gitconfig
 ln -s $HOME/nixos-config/dotfiles/xsession $HOME/.xsession
 ln -s $HOME/nixos-config/dotfiles/Xmodmap $HOME/.Xmodmap
-ln -s $HOME/nixos-config/dotfiles/Xresources $HOME/.Xresources
+ln -s $HOME/nixos-config/dotfiles/Xdefaults $HOME/.Xdefaults
 
 echo "Cloning neovim-config into $XDG_CONFIG_HOME/nvim..."
 git clone git@github.com:pederpus/neovim-config.git $XDG_CONFIG_HOME/nvim
