160614b664cae444f182241c16db3b1ca2fb145b
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-03 20:17:10 +0200

    Move dotfiles to separate folder

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 ln -s $HOME/nixos-config/configuration.nix /etc/nixos/configuration.nix
 ln -s /etc/nixos/hardware-configuration.nix $HOME/nixos-config/hardware-configuration.nix
+
+ln -s $HOME/nixos-config/dotfiles/gitconfig $HOME/.gitconfig
+ln -s $HOME/nixos-config/dotfiles/profile $HOME/.profile
