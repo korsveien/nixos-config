09a2ef543dd69247fde40622f1a30fd0a19fd563
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-04 15:55:20 +0200

    Add oh-my-zsh

--- pull-configs.sh
+++ pull-configs.sh
@@ -1,9 +1,4 @@
 #! /usr/bin/env sh
 
 git clone git@github.com:pederpus/neovim-config.git $XDG_CONFIG_HOME/nvim
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+git clone git@github.com:pederpus/oh-my-zsh.git $HOME/.oh-my-zsh
