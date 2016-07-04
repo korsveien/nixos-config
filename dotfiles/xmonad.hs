8610fcce855b4d4a8f9d7f81f8258f78fdab021c
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-04 17:50:19 +0200

    Moar shortcuts

--- dotfiles/xmonad.hs
+++ dotfiles/xmonad.hs
@@ -1,12 +1,16 @@
 import XMonad
 import XMonad.Layout.Spacing
+import XMonad.Actions.WindowGo
 import System.IO
+import XMonad.Util.EZConfig(additionalKeysP)
 
 -- put a 2px space around every window
 myLayout = spacing 15 $ Tall 1 (3/100) (1/2)
 
+
+-- key bindings
+
 main :: IO ()
 main = do xmonad $ defaultConfig { terminal = "termite"
-			         -- , modMask = mod4Mask
-				 , layoutHook = myLayout
-}
+	, layoutHook = myLayout
+	} `additionalKeysP` [ ("M-w", (runOrRaiseMaster "firefox" (className =? "Firefox"))) ]
