16f4c6c1efb5b0f2a0a19bc69053b54dec2359d0
Peder Korsveien <peder.korsveien@gmail.com>
2016-07-11 19:33:43 +0200

    Symlink gtk.css

--- symlinks.sh
+++ symlinks.sh
@@ -7,10 +7,10 @@ rm $HOME/.zshrc
 rm $HOME/.xsession
 rm $HOME/.Xmodmap
 rm $HOME/.Xdefaults
-rm $HOME/.fehbg
 rm $HOME/.xmonad/xmonad.hs
 rm $HOME/.xmobarrc
 rm $HOME/.ssh/config
+rm $XDG_CONFIG_HOME/gtk-3.0/gtk.css
 
 ln -s $HOME/nixos-config/dotfiles/ssh_config $HOME/.ssh/config
 ln -s $HOME/nixos-config/dotfiles/zshrc $HOME/.zshrc
@@ -18,8 +18,8 @@ ln -s $HOME/nixos-config/dotfiles/gitconfig $HOME/.gitconfig
 ln -s $HOME/nixos-config/dotfiles/xsession $HOME/.xsession
 ln -s $HOME/nixos-config/dotfiles/Xmodmap $HOME/.Xmodmap
 ln -s $HOME/nixos-config/dotfiles/Xdefaults $HOME/.Xdefaults
-ln -s $HOME/nixos-config/dotfiles/fehbg $HOME/.fehbg
 ln -s $HOME/nixos-config/dotfiles/xmobarrc $HOME/.xmobarrc
+ln -s $HOME/nixos-config/gtk/gtk.css $XDG_CONFIG_HOME/gtk-3.0/gtk.css
 
 rm -rf $HOME/.xmonad
 mkdir -p $HOME/.xmonad
